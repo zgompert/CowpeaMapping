7a236eb589c3dfb91568a43840f6142c749e714e
Zach Gompert <31893662+zgompert@users.noreply.github.com>
2021-05-23 14:07:29 -0600

    Add files via upload

---
+++ forkRunGemma1.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+#
+# core gemma fits for parameter estimation, bslmm and lmm 
+#
+
+use Parallel::ForkManager;
+my $max = 30;
+my $pm = Parallel::ForkManager->new($max);
+
+$g = shift(@ARGV); ## genotype file
+$p = shift(@ARGV); ## phenotype file
+
+foreach $ph (1..4){ ## four traits, starting at 2
+	$phn = $ph+1;
+	foreach $ch (0..9){
+		$pm->start and next;
+		$o = "o_cowpea1_fit_ph$ph"."_ch$ch";
+		if($ch == 0){ ## lmm only on chain 0
+			system "gemma -g $g -p $p -gk 1 -o $o\n";
+			system "gemma -g $g -p $p -lmm 4 -n $phn -k output/$o".".cXX.txt -o $o\n";
+		}
+    		system "gemma -g $g -p $p -bslmm 1 -n $phn -o $o -maf 0 -w 200000 -s 1000000\n";
+		$pm->finish;
+	}
+}
+$pm->wait_all_children;
+
---
+++ forkRunGemmaEpi2.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+#
+# core gemma fits for parameter estimation, bslmm and lmm 
+#
+
+use Parallel::ForkManager;
+my $max = 30;
+my $pm = Parallel::ForkManager->new($max);
+
+$g = shift(@ARGV); ## genotype file
+$p = shift(@ARGV); ## phenotype file
+
+foreach $ph (1..4){ ## four traits, starting at 2
+	$phn = $ph+1;
+	system "sleep 2\n";
+	foreach $ch (0..9){
+		$pm->start and next;
+		$o = "o_cowpea2_epiPc_ph$ph"."_ch$ch";
+	#	system "gemma -g $g -p $p -gk 1 -o $o\n";
+    		system "gemma -g $g -p $p -k output_stan/o_cowpea_fit_ph$ph"."_ch0.cXX.txt -bslmm 1 -n $phn -o $o -notsnp -w 200000 -s 1000000\n";
+    		#system "gemma -g $g -p $p -k output/$o".".cXX.txt -bslmm 1 -n $phn -o $o -notsnp -w 200000 -s 1000000\n";
+		$pm->finish;
+	}
+}
+$pm->wait_all_children;
+
